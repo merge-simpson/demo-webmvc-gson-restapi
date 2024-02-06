2004a0ba05494b8fc6d76e52e779dc4587c9a6d7
merge-simpson <letsdev@kakao.com>
2024-02-06 23:04:37 +0900

    feat(gson): timezone constraint: one of TimeZone.getAvailableIDs()

--- src/main/java/example/gson/properties/DemoGsonProperties.java
+++ src/main/java/example/gson/properties/DemoGsonProperties.java
@@ -1,8 +1,18 @@
 package example.gson.properties;
 
+import jakarta.validation.ValidationException;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 
+import java.util.TimeZone;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @param pretty JSON 직렬화 시 이쁘게 보여 줄 건지 ^^
+ * @param timeZone 타임존은 예를 들어 "Asia/Seoul" 등에 해당함.
+ */
 @ConfigurationProperties("app.gson")
 @ConfigurationPropertiesBinding
 public record DemoGsonProperties(
@@ -16,6 +26,31 @@ public record DemoGsonProperties(
 
         if (timeZone == null || timeZone.isBlank()) {
             timeZone = "Asia/Seoul";
+        } else {
+            // stream을 두 차례 이상 사용하기 위해.
+            Supplier<Stream<String>> allTimeZoneSupplier =
+                    () -> Stream.of(TimeZone.getAvailableIDs());
+            String tz = timeZone; // cuz' stream requires: (명시적인) local 변수 or final 변수
+            if (allTimeZoneSupplier.get().noneMatch((item) -> item.equals(tz))) {
+                String listString = allTimeZoneSupplier.get()
+                        .reduce(
+                                "",
+                                (old, cur) ->
+                                        STR."""
+                                        \{old}
+                                        ❣️\{cur}
+                                        """
+                        );
+
+                String message = STR."""
+                        Time zone of \{timeZone} is not valid time zone.
+
+                        Choose one of them:
+                        \{listString}
+                        """;
+
+                throw new ValidationException(message);
+            }
         }
     }
 }
